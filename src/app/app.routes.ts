6e22dcb61e3afe5cc1cf3bfe6da4ff0ea309b0a0
devcom33 <126575832+devcom33@users.noreply.github.com>
2026-07-10 22:05:31 +0100

    feat: Define application routes for dashboard, transaction form, and stats summary

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,20 @@
 import { Routes } from '@angular/router';
+import { TransactionForm } from './components/transaction-form/transaction-form';
+import { DashboardShell } from './components/dashboard-shell/dashboard-shell';
+import { StatsSummary } from './components/stats-summary/stats-summary';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+    {
+        path: '',
+        component: DashboardShell
+    },
+    {
+        path: 'add-transaction',
+        component: TransactionForm
+    },
+    {
+        path: 'stats',
+        component: StatsSummary
+    }
+];
